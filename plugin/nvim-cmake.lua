c91e98f4c158f99f1f01b631763d1419c3a8a9e4
kevin hart <me@mkitcc.com>
2021-06-16 16:57:59 +0800

    vim to lua

--- plugin/nvim-cmake.lua
+++ plugin/nvim-cmake.lua
@@ -1,5 +1,3 @@
-lua << EOF
 vim.cmd "command! CmakeBuild lua require'nvim-cmake'.build()"
 vim.api.nvim_set_keymap('n','<Leader>r',":lua require'nvim-cmake'.build()<Enter>",{noremap = true,nowait =true})
-EOF
 
